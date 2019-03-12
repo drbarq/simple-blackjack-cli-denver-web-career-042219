d534cdf00edf58bb6f2b3413cb8d6e5771c59393
Joe Tustin <j.tustin@gmail.com>
2019-03-12 17:54:00 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -56,7 +56,7 @@ def runner
   welcome
   current_total = initial_round
     until current_total > 21
-      current_total = hit?(current_total)
+      hit?(current_total) = current_total
   end
   return end_game(current_total)
 end
