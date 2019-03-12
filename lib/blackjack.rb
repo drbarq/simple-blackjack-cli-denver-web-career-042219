f97e6f3b7e118016dcff80e15db237762314b7c9
Joe Tustin <j.tustin@gmail.com>
2019-03-12 17:14:05 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,9 +33,8 @@ end
 
 def hit?(card_total)
   prompt_user
-##  get_user_input
   if get_user_input == "h"
-    puts deal_card + card_total
+  ##  puts deal_card + card_total
     return deal_card + card_total
   elsif get_user_input == "s"
   ##  puts card_total
